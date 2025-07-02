ea1fb2ff9b2c0ad169e935b3acee9b326082a1fe
emaduddinsurgyy <emaduddin@surgyy.com>
2025-07-02 11:01:53 +0530

    Initial commit from emaduddinsurgyy

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-RES10_PROTO_PATH = "models/deploy.prototxt.txt"
-RES10_MODEL_PATH = "models/res10_300x300_ssd_iter_140000.caffemodel"
+import os
+
+RES10_PROTO_PATH = os.getenv("RES10_PROTO_PATH", "models/deploy.prototxt.txt")
+RES10_MODEL_PATH = os.getenv("RES10_MODEL_PATH", "models/res10_300x300_ssd_iter_140000.caffemodel")
